8a92db3a30d1480e7755d3564fcf09dfc96f4926
sarvin nami <78687226+srvn-nm@users.noreply.github.com>
2021-08-13 16:16:12 +0430

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,8 @@
+# author : Sarvin Nami
+num = int(input('Please enter the last member of you range here : '))
+a = 0
+i = 1
+while i <= num :
+    a += i
+    i += 1
+print(f'sum of range is = {a}')
